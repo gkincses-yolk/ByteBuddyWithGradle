1ad2497cfe0f05864c8b08a0491929bf932b3a1d
Gabor Kincses <gkincses@yolk.digital>
2021-10-14 23:21:12 +0200

    Compile time transformation works in maven

--- app/src/main/java/MyApp.java
+++ app/src/main/java/MyApp.java
@@ -1,46 +1,62 @@
 import digital.yolk.MyClass;
 import digital.yolk.extension.ExtensionPoint1;
-import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodDelegation;
 
-import java.lang.reflect.*;
+import java.lang.reflect.InvocationTargetException;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public class MyApp {
 
-    Class<?> completeClass;
-    MyClass completeObject;
+    public static abstract class InvocationStrategy {
 
-    public MyApp() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        completeClass = this.buildClass();
-        completeObject = this.instantiate();
+        Class<? extends MyClass> completeClass;
+        MyClass completeObject;
+
+        public InvocationStrategy() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        }
+
+        protected MyClass instantiate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+            return completeClass.getConstructor().newInstance();
+        }
+
+        public void executeGeneratedMethod() {
+            System.out.println(completeObject.toMyString());
+        }
+    }
+    public static class RuntimeAugmentation extends InvocationStrategy {
+
+        public RuntimeAugmentation() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+            completeClass = this.buildClass();
+            completeObject = this.instantiate();
+        }
+
+        private Class<? extends MyClass> buildClass() {
+            DynamicType.Unloaded<? extends MyClass> unloadedType = new ByteBuddy()
+                    .subclass(MyClass.class)
+                    .method(named("toMyString"))
+                    .intercept(MethodDelegation.to(new ExtensionPoint1()))
+                    .make();
+            return unloadedType.load(MyApp.class.getClassLoader()).getLoaded();
+        }
+
     }
 
-    private Class<?> buildClass() {
-        DynamicType.Unloaded unloadedType = new ByteBuddy()
-                .subclass(MyClass.class)
-                .method(named("toMyString"))
-                .intercept(MethodDelegation.to(new ExtensionPoint1()))
-                .make();
-        Class<?> completeMyClass = unloadedType.load(MyApp.class.getClassLoader()).getLoaded();
-        return completeMyClass;
+    public static class DynamicInvocation extends InvocationStrategy {
+
+        public DynamicInvocation() throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+            completeClass = (Class<? extends MyClass>) MyApp.class.getClassLoader().loadClass("digital.yolk.MyClass");
+            completeObject = this.instantiate();
+        }
+
     }
 
-    private MyClass instantiate() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        Constructor ctor = completeClass.getConstructor();
-        MyClass completeObject = (MyClass) ctor.newInstance();
-        return completeObject;
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException, ClassNotFoundException {
+//        InvocationStrategy strategy = new RuntimeAugmentation();
+        InvocationStrategy strategy = new DynamicInvocation();
+        strategy.executeGeneratedMethod();
     }
 
-    private void executeGeneratedMethod() {
-        System.out.println(completeObject.toMyString());
-    }
-
-    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        MyApp app = new MyApp();
-        app.executeGeneratedMethod();
-    }
 }
--- core-main/src/main/java/digital/yolk/TransformerPlugin.java
+++ core-main/src/main/java/digital/yolk/TransformerPlugin.java
@@ -2,41 +2,31 @@ package digital.yolk;
 
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.build.Plugin;
+import net.bytebuddy.description.modifier.ModifierContributor;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.*;
-import net.bytebuddy.dynamic.scaffold.InstrumentedType;
-import net.bytebuddy.dynamic.scaffold.inline.*;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.jar.asm.Opcodes;
+import net.bytebuddy.matcher.ElementMatchers;
 
 import java.io.IOException;
 
 @Slf4j
 public class TransformerPlugin implements Plugin {
     @Override
+    public boolean matches(TypeDescription target) {
+        System.out.println("target=" + target.getName());
+        return target.getName().equals("digital.yolk.MyClass");
+    }
+
+    @Override
     public DynamicType.Builder<?> apply(DynamicType.Builder<?> builder, TypeDescription typeDescription, ClassFileLocator classFileLocator) {
-        if (typeDescription.represents(MyClass.class)) {
-            log.warn("Let's work on this class: " + String.valueOf(typeDescription));
-//            return new RebaseDynamicTypeBuilder<>(
-//                    new InstrumentedType.WithFlexibleName().withMethod(),
-//
-//            );
-            return null;
-        } else {
-            log.warn("Ignoring class: " + String.valueOf(typeDescription));
-            return null;
-        }
+        return builder.modifiers(ModifierContributor.ForType.EMPTY_MASK | Opcodes.ACC_PUBLIC).method(ElementMatchers.named("toMyString")).intercept(FixedValue.value("Hello World!!!"));
     }
 
     @Override
     public void close() throws IOException {
 
     }
-
-    @Override
-    public boolean matches(TypeDescription target) {
-        if (target.represents(MyClass.class)) {
-            return true;
-        }
-
-        return false;
-    }
 }
+
